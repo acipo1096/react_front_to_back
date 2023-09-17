69854f1b70dcd9a5f42c240ac31f31a285b03c80
Alex Cipollone <alex.cipollone10@gmail.com>
2023-09-17 14:34:08 -0400

    working with conditionals

--- src/App.js
+++ src/App.js
@@ -10,19 +10,29 @@ function App() {
         {id: 3, text: 'Comment three'},
     ]
 
+    const loading = false;
+
+    const commentBlock = (
+        <div className="comments">
+            <h3>Comments ({comments.length})</h3>
+            <ul>
+                {comments.map((comment,index)=>(
+                    <li key={index}>{comment.text}</li>
+                ))}
+            </ul>
+        </div>
+    )
+
+    const showComments = true;
+
+    if (loading) return <h1>Loading...</h1>
+
     return (
         <div className="container">
             <h1>{title.toUpperCase()}</h1>
             <p>{body}</p>
-            
-            <div className="comments">
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                    {comments.map((comment,index)=>(
-                        <li key={index}>{comment.text}</li>
-                    ))}
-                </ul>
-            </div>
+
+            {showComments && commentBlock}
         </div>
     )
 }
