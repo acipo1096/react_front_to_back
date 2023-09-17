eb3f47b7f507b4d63002905cb9c8a6dbbe8b5522
Alex Cipollone <alex.cipollone10@gmail.com>
2023-09-17 14:34:57 -0400

    completed 'Conditionals'

--- src/App.js
+++ src/App.js
@@ -2,37 +2,9 @@
 // A fragment is just an empty angle bracket
 
 function App() {
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'},
-    ]
-
-    const loading = false;
-
-    const commentBlock = (
-        <div className="comments">
-            <h3>Comments ({comments.length})</h3>
-            <ul>
-                {comments.map((comment,index)=>(
-                    <li key={index}>{comment.text}</li>
-                ))}
-            </ul>
-        </div>
-    )
-
-    const showComments = true;
-
-    if (loading) return <h1>Loading...</h1>
-
     return (
         <div className="container">
-            <h1>{title.toUpperCase()}</h1>
-            <p>{body}</p>
-
-            {showComments && commentBlock}
+            <h1>My App</h1>
         </div>
     )
 }
