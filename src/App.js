36e322e05c9003631b7f5bb12f318f7ebc71b7f9
Alex Cipollone <alex.cipollone10@gmail.com>
2023-09-18 13:09:13 -0400

    completed "Global State - Feedback Items"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 // This is the main app component
 // A fragment is just an empty angle bracket
+import { useState } from 'react'
 import Header from "./components/Header"
-import FeedbackItem from "./components/FeedbackItem"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackData from './data/FeedbackData'
 
 function App() {
+    const [feedback, setFeedback] = useState(FeedbackData)
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackItem />
+                <FeedbackList feedback={feedback}/>
             </div>
         </>
 
