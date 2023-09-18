ee56cb5ca91c5b0ee314ff89f6b0da9c6aae9c5b
Alex Cipollone <alex.cipollone10@gmail.com>
2023-09-18 12:34:03 -0400

    completed "Components, Props & Prop Types"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 // This is the main app component
 // A fragment is just an empty angle bracket
+import Header from "./components/Header"
 
 function App() {
     return (
-        <div className="container">
-            <h1>My App</h1>
-        </div>
+        <>
+            <Header text={true}/>
+            <div className="container">
+                <h1>My App</h1>
+            </div>
+        </>
+
     )
 }
 
