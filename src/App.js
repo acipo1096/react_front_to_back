547a7519149a86979e1f0ba6bce4464320f4f60f
Alex Cipollone <alex.cipollone10@gmail.com>
2023-09-23 08:45:49 -0400

    Completed "Updating to React Router v6"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // This is the main app component
 // A fragment is just an empty angle bracket
 import {v4 as uuidv4} from 'uuid'
-import { BrowserRouter as Router,Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useState } from 'react'
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
@@ -28,13 +28,22 @@ function App() {
         <Router>
             <Header />
             <div className="container">
-                <Route exact path='/'>
-                <FeedbackForm handleAdd={addFeedback}/>
-                <FeedbackStats feedback={feedback}/>
-                <FeedbackList feedback={feedback}
-                handleDelete={deleteFeedback}/>
+                <Routes>
+                    <Route 
+                        exact
+                        path='/'
+                        element={
+                            <>
+                                <FeedbackForm handleAdd={addFeedback}/>
+                                <FeedbackStats feedback={feedback}/>
+                                <FeedbackList feedback={feedback}
+                                handleDelete={deleteFeedback}/>
+                            </>
+                        
+                    }>
                 </Route>
-            <Route path='/about' component={AboutPage} />
+                <Route path='/about' element={<AboutPage />} />
+                </Routes>
             </div>
         </Router>
 
