ff958208103aac0d3ed92b19888b72a07fe9aa82
Alex Cipollone <alex.cipollone10@gmail.com>
2023-09-23 08:41:36 -0400

    completed "Creating Routes"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // This is the main app component
 // A fragment is just an empty angle bracket
 import {v4 as uuidv4} from 'uuid'
+import { BrowserRouter as Router,Route } from 'react-router-dom'
 import { useState } from 'react'
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutPage from './pages/AboutPage'
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -23,15 +25,18 @@ function App() {
     }
 
     return (
-        <>
+        <Router>
             <Header />
             <div className="container">
+                <Route exact path='/'>
                 <FeedbackForm handleAdd={addFeedback}/>
                 <FeedbackStats feedback={feedback}/>
                 <FeedbackList feedback={feedback}
                 handleDelete={deleteFeedback}/>
+                </Route>
+            <Route path='/about' component={AboutPage} />
             </div>
-        </>
+        </Router>
 
     )
 }
